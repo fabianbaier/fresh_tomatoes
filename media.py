0d701c272b086386386f2bcd747c7e3a67fa9887
Fabian Baier <fabian.baier1987@gmail.com>
2015-09-12 15:17:02 -0700

    Create media.py

---
+++ media.py
@@ -0,0 +1,10 @@
+class Movie():
+    def __init__(self, movie_title, movie_storyline, movie_more, poster_image, trailer_youtube):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.more = movie_more
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
