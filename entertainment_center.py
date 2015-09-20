7f398feebb5c8b7e48f10a5877c66865d78506b1
Fabian Baier <fabian.baier1987@gmail.com>
2015-09-19 21:25:36 -0700

    added more comments and fixed formatting issue

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,24 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # Importing my other classes and functions
 # fresh_tomatoes.py is used for creating the fresh_tomatoes.html
 import fresh_tomatoes
-# media.py is used for defining the class Movie() and interpreting the passed list as well as adding trailer display functionality
 import media
 
-# Defining  my list
-man_on_fire = media.Movie("Man on Fire", "In a Mexico City wracked by a recent wave of kidnappings, ex-CIA operative John Creasy (Denzel Washington) reluctantly accepts a job as a bodyguard for 9-year-old Lupita (Dakota Fanning), the daughter of wealth…","http://www.imdb.com/title/tt0328107/","https://upload.wikimedia.org/wikipedia/en/e/e8/Man_on_fireposter.jpg","https://www.youtube.com/watch?v=g4kLizDXLY0")
+# Create list of my six favorite movies by instantiating 6 movie classes
+man_on_fire = media.Movie("Man on Fire", "Denzel on vendetta as a bodyguard","http://www.imdb.com/title/tt0328107/","https://upload.wikimedia.org/wikipedia/en/e/e8/Man_on_fireposter.jpg","https://www.youtube.com/watch?v=g4kLizDXLY0")
 
-inside_out = media.Movie("Inside Out", "Riley (Kaitlyn Dias) is a happy, hockey-loving 11-year-old Midwestern girl, but her world turns upside-down when she and her parents move to San Francisco. Riley's emotions -- led by Joy (Amy Poehler) -- try to guide her through this difficult, life-changing event. However, the stress of the move br…","http://www.imdb.com/title/tt2096673/","https://upload.wikimedia.org/wikipedia/en/0/0a/Inside_Out_%282015_film%29_poster.jpg","https://www.youtube.com/watch?v=_MC3XuMvsDI")
+inside_out = media.Movie("Inside Out", "Riley moves to San Francisco","http://www.imdb.com/title/tt2096673/","https://upload.wikimedia.org/wikipedia/en/0/0a/Inside_Out_%282015_film%29_poster.jpg","https://www.youtube.com/watch?v=_MC3XuMvsDI")
 
-jurassic_world = media.Movie("Jurassic World", "Located off the coast of Costa Rica, the Jurassic World luxury resort provides a habitat for an array of genetically engineered dinosaurs, including the vicious and intelligent Indominus rex. When the massive creature escapes, it sets off a chain reaction that causes the other dinos to run amok. Now…","http://www.imdb.com/title/tt0369610/","https://upload.wikimedia.org/wikipedia/en/6/6e/Jurassic_World_poster.jpg","https://www.youtube.com/watch?v=RFinNxS5KN4")
+jurassic_world = media.Movie("Jurassic World", "Dinosaurs are back","http://www.imdb.com/title/tt0369610/","https://upload.wikimedia.org/wikipedia/en/6/6e/Jurassic_World_poster.jpg","https://www.youtube.com/watch?v=RFinNxS5KN4")
 
-star_wars = media.Movie("Star Wars", "The further adventures of Luke Skywalker (Mark Hamill), Han Solo (Harrison Ford) and Princess Leia (Carrie Fisher)...","http://www.imdb.com/title/tt2488496/","https://upload.wikimedia.org/wikipedia/en/e/e6/Star_Wars_The_Force_Awakens_Teaser_Poster.jpg","https://www.youtube.com/watch?v=wCc2v7izk8w")
+star_wars = media.Movie("Star Wars", "May the force be with you","http://www.imdb.com/title/tt2488496/","https://upload.wikimedia.org/wikipedia/en/e/e6/Star_Wars_The_Force_Awakens_Teaser_Poster.jpg","https://www.youtube.com/watch?v=wCc2v7izk8w")
 
-mission_impossible_five = media.Movie("Mission Impossible 5", "With the IMF now disbanded and Ethan Hunt (Tom Cruise) out in the cold, a new threat -- called the Syndicate -- soon emerges. The Syndicate is a network of highly skilled operatives who are dedicated to establishing a new world order via an escalating series of terrorist attacks. Faced with what may…","http://www.imdb.com/title/tt2381249/","https://upload.wikimedia.org/wikipedia/en/f/fb/Mission_Impossible_Rogue_Nation_poster.jpg","https://www.youtube.com/watch?v=gOW_azQbOjw")
+mission_impossible_five = media.Movie("Mission Impossible 5", "May the force be with you","http://www.imdb.com/title/tt2381249/","https://upload.wikimedia.org/wikipedia/en/f/fb/Mission_Impossible_Rogue_Nation_poster.jpg","https://www.youtube.com/watch?v=gOW_azQbOjw")
 
-spectre = media.Movie("Spectre", "A cryptic message from his past sends James Bond (Daniel Craig) on a quest to uncover a sinister organization while M (Ralph Fiennes) battles political forces that want to shut down the British secret service...","http://www.imdb.com/title/tt2379713/","http://ia.media-imdb.com/images/M/MV5BMjIwNTA1MDA2Ml5BMl5BanBnXkFtZTgwNzIzMTA5NDE@._V1_SX640_SY720_.jpg","https://www.youtube.com/watch?v=LTDaET-JweU")
+spectre = media.Movie("Spectre", "British Secret service...","http://www.imdb.com/title/tt2379713/","http://ia.media-imdb.com/images/M/MV5BMjIwNTA1MDA2Ml5BMl5BanBnXkFtZTgwNzIzMTA5NDE@._V1_SX640_SY720_.jpg","https://www.youtube.com/watch?v=LTDaET-JweU")
 
 # Creating the list
 movies = [man_on_fire, inside_out, jurassic_world, star_wars, mission_impossible_five, spectre]
 
 # Passing the list to the open_movies_page function with parameter 'movies'
+# Generate HTML for static web page and launch
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,12 +1,25 @@
-# Creating the Movie Class and the parameters
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Creating the Movie Class and initiate with 5 attributes:
+# Movie Title, Storyline, More, Poster Image, Youtube Tailer
 class Movie():
+    """ ... this is the class documentation docstring ...
+    this class creates a template for a movie webpage
+    """
     def __init__(self, movie_title, movie_storyline, movie_more, poster_image, trailer_youtube):
+        """ ... this is the constructor method docstring ...
+        I added self.more and modified it in the fresh_tomatoes.py
+        to have a better description of the movies playing
+        """
         self.title = movie_title
         self.storyline = movie_storyline
         self.more = movie_more
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
+# Create function to launch youtube trailer. Launched on click from static website.
     def show_trailer(self):
-    # Passing the url to the webbrowser
+        """ show_trailer() docstring...
+        lets the magic happen and open this embedded youtube trailer for the selected movie
+        """
         webbrowser.open(self.trailer_youtube_url)
